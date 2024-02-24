2695cdcbaee9adf690f54bfae943487820b0168c
richie <richie.kwon1@gmail.com>
2024-02-24 15:10:04 +0000

    created the swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('yonder board')
+        .setDescription('yonder board APIs')
+        .setVersion('1.0')
+        .addTag('board')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document) // localhost:3000/api
+
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()
