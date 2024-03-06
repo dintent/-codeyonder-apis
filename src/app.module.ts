eb75dda4d551145ca936fac29be8036f746b9e56
richie <richie.kwon1@gmail.com>
2024-03-06 15:28:48 +0000

    user dto created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { BoardModule } from './board/board.module'
 import { LoggingMiddleware } from './middleware/logging.midleware'
-import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module'
 import ConfigModule from './config'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-    imports: [BoardModule, ConfigModule(), UserModule], //Configuration module is a dynamic module so it must be used with brackets
+    imports: [
+        BoardModule,
+        ConfigModule(),
+        UserModule,
+        MongooseModule.forRoot(process.env.MONGO_URL),
+    ], //Configuration module is a dynamic module so it must be used with brackets
     controllers: [AppController],
     providers: [AppService],
 })
