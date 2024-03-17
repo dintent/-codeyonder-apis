9aa0af17accaf76018226c1aea2216f02d294ef0
richie <richie.kwon1@gmail.com>
2024-03-17 17:02:32 +0000

    tab

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule)
 
-    const config = new DocumentBuilder()
-        .setTitle('yonder board')
-        .setDescription('yonder board APIs')
-        .setVersion('1.0')
-        .addTag('board') // the mapping to the one in @ApiTags in the board controller
-        .build()
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('api', app, document) // localhost:3000/api
+  const config = new DocumentBuilder()
+    .setTitle('yonder board')
+    .setDescription('yonder board APIs')
+    .setVersion('1.0')
+    .addTag('board') // the mapping to the one in @ApiTags in the board controller
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document) // localhost:3000/api
 
-    await app.listen(3000)
+  await app.listen(3000)
 }
 bootstrap()
