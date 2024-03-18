1e0b09ee4d26699f45f06507baf443db5aa6b27f
richie <richie.kwon1@gmail.com>
2024-03-18 19:36:04 +0000

    user schema is created

--- src/board/schemas/board.schema.ts
+++ src/board/schemas/board.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsString } from 'class-validator'
 import { Document } from 'mongoose'
 
@@ -6,9 +7,11 @@ export type BoardDocument = Board & Document
 
 @Schema({ timestamps: true })
 export class Board extends Document {
+  @ApiProperty({ description: 'name', example: 'cathy' })
   @Prop({ required: true })
   name: string
 
+  @ApiProperty({ description: 'contents', example: 'please write here' })
   @Prop({ required: true })
   @IsString()
   @IsNotEmpty()
--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,17 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { ApiProperty } from '@nestjs/swagger'
 import { Document } from 'mongoose'
 
 export type UserDocument = User & Document
 
 @Schema({ timestamps: true })
 export class User extends Document {
-  @Prop({ unique: true })
-  username: string
+  // @Prop({ unique: true })
+  // @ApiProperty({ description: 'username', example: 'cathy' })
+  // username: string
 
+  @ApiProperty({ description: 'password', example: '1qa2ws' })
   @Prop()
   password: string
 
-  @Prop()
+  @ApiProperty({ description: 'name', example: 'id' })
+  @Prop({ unique: true })
   name: string
 }
 
